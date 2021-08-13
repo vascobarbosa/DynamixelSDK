5130189623fc919928054784a9ccb0ecd58e5f7d
Vasco Barbosa <vaskobarbosa@gmail.com>
2021-08-13 10:46:32 +0100

    last

--- python/tests/protocol2_0/read_write.py
+++ python/tests/protocol2_0/read_write.py
@@ -101,7 +101,7 @@ DXL_ID                      = 1
 
 # Use the actual port assigned to the U2D2.
 # ex) Windows: "COM*", Linux: "/dev/ttyUSB*", Mac: "/dev/tty.usbserial-*"
-DEVICENAME                  = '/dev/ttyUSB0'
+DEVICENAME                  = '/dev/cu.usbserial-FT5NUTLL'
 
 TORQUE_ENABLE               = 1     # Value for enabling the torque
 TORQUE_DISABLE              = 0     # Value for disabling the torque
